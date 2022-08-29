ff4a23635811a6a7b13c11ef02abb9d4f976bb0b
AlanFelixDEV <112443266+AlanFelixDEV@users.noreply.github.com>
2022-08-29 20:51:17 -0300

    Update index.ts

--- app/index.ts
+++ app/index.ts
@@ -4,10 +4,12 @@ import { rotas } from './rotas';
 
 import "dotenv/config";
 
+const PORT = 5001;
+
 const servidor = express();
 servidor.use(cors());
 servidor.use(express.json());
 servidor.use(rotas);
 
 
-servidor.listen(5001, () => { console.log("Servidor rodando na porta 5001")});
+servidor.listen(PORT, () => { console.log("Servidor rodando na porta 5001")});
