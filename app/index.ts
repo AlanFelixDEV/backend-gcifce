88de777fbf20e945a3b2b270caa913b4095f5069
Alan <alanfelixds@gmail.com>
2022-08-30 08:32:28 -0300

    subindo pro heroku

--- app/index.ts
+++ app/index.ts
@@ -5,9 +5,10 @@ import { rotas } from './rotas';
 import "dotenv/config";
 
 const servidor = express();
+const port = process.env.PORT || 5001
 servidor.use(cors());
 servidor.use(express.json());
 servidor.use(rotas);
 
 
-servidor.listen(5001, () => { console.log("Servidor rodando na porta 5001")});
+servidor.listen(port, () => { console.log(`Rodando na porta ${port}`)});
